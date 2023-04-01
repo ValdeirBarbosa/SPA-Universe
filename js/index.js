12c218a3dce2c86cb1a09d4530cf5b62bf437576
valdeir.barbosa <valdeir.barbosa@outlook.com>
2023-03-31 22:33:44 -0300

    ajustando rotas

--- js/index.js
+++ js/index.js
@@ -1,9 +1,15 @@
-import {Router} from "./router.js"
+import { Router } from "./router.js"
+
+
+
+const listMenu = document.querySelector('ul li a')
+console.log(listMenu)
+
 
 const router = new Router()
 router.add("/", "/pages/home.html")
-router.add("/about", "/pages/about.html")
-router.add("/contact", "/pages/contact.html")
+router.add("/universo", "/pages/universo.html")
+router.add("/explorar", "/pages/explorar.html")
 router.add(404, "/pages/404.html")
 
 router.handle()
--- js/router.js
+++ js/router.js
@@ -3,22 +3,19 @@ export class Router {
   routes = {}
   add(routeName, page) {
     this.routes[routeName] = page
-
   }
   route(event) {
     event = event || window.event
     event.preventDefault()
-
-
     window.history.pushState({}, "", event.target.href)
+    console.log(event.target.pathname)
     event.preventDefault();
     this.handle()
   }
   handle() {
     const { pathname } = window.location
     const route = this.routes[pathname] || this.routes[404]
-    console.log(this.pathname)
-
+   
     fetch(route)
       .then((data) => data.text())
       .then((html) => {
