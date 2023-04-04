8898a989db17740123d0e1338ed4ce0ab2411829
valdeir.barbosa <valdeir.barbosa@outlook.com>
2023-04-03 22:40:56 -0300

    ajustes finais e dinamica de background

---
+++ js/elements.js
@@ -0,0 +1,9 @@
+const body = document.querySelector('body')
+const listMenu = document.querySelector('ul li')
+const imageBgr = {
+  "/": "../assets/mountains-universe-1.png",
+  "/universo": "../assets/mountains-universe-2.png",
+  "/explorar": "../assets/mountains-universe-3.png"
+}
+
+export { body,listMenu, imageBgr}
--- js/router.js
+++ js/router.js
@@ -1,3 +1,4 @@
+import { body, imageBgr } from "./elements.js"
 
 export class Router {
   routes = {}
@@ -5,23 +6,20 @@ export class Router {
     this.routes[routeName] = page
   }
   route(event) {
-    event = event || window.event
     event.preventDefault()
+    event = event || window.event
     window.history.pushState({}, "", event.target.href)
-    console.log(event.target.pathname)
-    event.preventDefault();
     this.handle()
   }
   handle() {
     const { pathname } = window.location
     const route = this.routes[pathname] || this.routes[404]
-   
+    body.style.backgroundImage = `url(${imageBgr[pathname]})`
     fetch(route)
       .then((data) => data.text())
       .then((html) => {
         document.querySelector("#app").innerHTML = html
       })
   }
-
 }
 
